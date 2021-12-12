b7e9bf5c65f97495ef979bfac0be7183ac5c038d
richard <3025017591@qq.com>
2021-12-12 13:40:32 +0800

    merge sort 添加文件

---
+++ OJ/src/main/java/sort/HeapSort.java
@@ -0,0 +1,8 @@
+package sort;
+
+public class HeapSort extends SortHelperSimpleImpl {
+    @Override
+    public void sort(int[] nums) {
+
+    }
+}
---
+++ OJ/src/main/java/sort/MergeSort.java
@@ -0,0 +1,8 @@
+package sort;
+
+public class MergeSort extends SortHelperSimpleImpl {
+    @Override
+    public void sort(int[] nums) {
+
+    }
+}
