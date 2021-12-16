85b7fdec937dab35882e3f747f28c8db5d87ae94
Richard <maoyuching@qq.com>
2021-12-17 02:09:13 +0800

    add function

--- OJ/src/main/java/sort/MergeSort.java
+++ OJ/src/main/java/sort/MergeSort.java
@@ -3,6 +3,9 @@ package sort;
 public class MergeSort extends SortHelperSimpleImpl {
     @Override
     public void sort(int[] nums) {
+    }
+
+    public void mergeSort(int[] nums, int b1, int b2, int e1, int e2) {
 
     }
 }
