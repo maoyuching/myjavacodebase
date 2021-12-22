fbd00818b5961ba75e475875bcfc0bf8be319060
Richard <18270414620@139.com>
2021-12-22 16:22:59 +0000

    commit on gitpod first

---
+++ OJ/src/main/java/tree/Solution.java
@@ -0,0 +1,28 @@
+
+ class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+
+public class Solution {
+    public int TreeDepth(TreeNode root) {
+        System.out.println("hi");
+        return 1;
+    }
+
+    public static void main(String[] a){
+        System.out.println("hi");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("hi");
+    }
+}
